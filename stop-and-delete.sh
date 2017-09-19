193a19be266f80e24d02710ac1e35dad31dd6318
Carlos Alandete <carlos.alandete@gmail.com>
2017-09-19 12:15:03 +0200

    Drop refactoring bomb

--- stop-and-delete.sh
+++ stop-and-delete.sh
@@ -29,37 +29,21 @@ echo ""
 echo "               Kubernetes-for-Symfony"
 echo ""
 echo ""
-sleep 1
-echo "CREATING PERSISTENT VOLUMES..."
+echo "STOPPING..."
 echo ""
+sleep 1
 
-if [[ $(minikube status | grep 'minikube: Stopped') == 'minikube: Stopped' ]]
-then
-
-  minikube start
-  echo ""
-
+if [[ $(minikube status | grep 'minikube: Stopped') == 'minikube: Stopped' ]]; then
+  echo "ERROR: Minikube is not running"
 fi
 
-if [[ $(minikube status | grep 'minikube: Running') == 'minikube: Running' ]]
-then
-
-  for yaml in kubernetes/volumes/*.yaml
-  do
-    kubectl create -f $yaml
-  done
-
-  sleep 1
+if [[ $(minikube status | grep 'minikube: Running') == 'minikube: Running' ]]; then
+  kubectl delete deployments --all
+  kubectl delete services --all
+  kubectl delete statefulsets --all
+  kubectl delete pods --all
   echo ""
+  sleep 5
+  minikube stop
   echo ""
-  echo "PERSISTENT VOLUMES:"
-  echo ""
-  kubectl get persistentvolumes
-  echo ""
-  echo ""
-  echo "PERSISTENT VOLUME CLAIMS:"
-  echo ""
-  kubectl get persistentvolumeclaims
-  echo ""
-
 fi
